7727b654caa212ed5ee06e440ea547ab088580f4
RuanSR <ruansilvarosa@gmail.com>
2021-08-05 23:39:17 -0300

    adicionado configs eslint, prettier e editorConfig

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./assets/App.css";
+import React from 'react';
+import './assets/App.css';
 
-import Books from "./components/Books";
-import Header from "./components/Header";
-
+import Books from './components/Books';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Books/>
+      <Header />
+      <Books />
     </div>
   );
 }
